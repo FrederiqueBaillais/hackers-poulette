a13dc29dcd005bfa9be54c7afb42ee66a0d53c46
Frederique Baillais <frederique.baillais@gmail.com>
2021-03-23 16:51:34 +0100

    php fonctionne pas

--- index.php
+++ index.php
@@ -78,7 +78,7 @@ if (isset($_POST['firstname'])){ //les variables sont déjà crées
         //Instantiation and passing `true` enables exceptions
         $mail = new PHPMailer(true);
 
-        try {
+        /*try {
             $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
             $mail->IsSMTP();                                            //Send using SMTP
             $mail->Mailer = "smtp";
@@ -86,10 +86,9 @@ if (isset($_POST['firstname'])){ //les variables sont déjà crées
             $mail->SMTPSecure = "PHPMailer::ENCRYPTION_SMTPS";          //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
             $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
             $mail->Host       = "smtp.gmail.com";                       //Set the SMTP server to send through
-            $mail->Username   = "fred.bail.becode@gmail.com";           //SMTP username
-            $mail->Password   = "t7uG@nrW2oYp5*fAAKHu";                 //SMTP password
+            $mail->Username   = "fred.bail.becode@yopmail.com";         //SMTP username pas de mot de passe pour yopmail
             
-            $mail->AddAddress("fred.bail.becode@gmail.com", "Fred Bail");
+            $mail->AddAddress("fred.bail.becode@yopmail.com", "Fred Bail");
             $mail->SetFrom("$email", "$firstname $lastname");
             
             $mail->IsHTML(true);
@@ -99,7 +98,7 @@ if (isset($_POST['firstname'])){ //les variables sont déjà crées
             echo 'Message has been sent';
         } catch (Exception $e) {
             echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-        }
+        }*/
 
 	} else { // 1ère fois qu'on on accède à la page
 		$firstname = "";
