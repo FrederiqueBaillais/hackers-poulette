458d8e95eeecbb9647e79beb0eeacde4bed178a3
Frederique Baillais <frederique.baillais@gmail.com>
2021-03-24 11:30:34 +0100

    gmail

--- index.php
+++ index.php
@@ -93,10 +93,11 @@ if(isset($_POST['fake-field']) && $_POST['fake-field'] != '') {
                     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
                     $mail->SMTPSecure = "PHPMailer::ENCRYPTION_SMTPS";          //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
                     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-                    $mail->Host       = "smtp.yopmail.com";                     //Set the SMTP server to send through
-                    $mail->Username   = "fred.bail.becode@yopmail.com";         //SMTP username
+                    $mail->Host       = "smtp.gmail.com";                     //Set the SMTP server to send through
+                    $mail->Username   = "fred.bail.becode@gmail.com";         //SMTP username
+                    $mail->Password   = "t7uG@nrW2oYp5*fAAKHu";
 
-                    $mail->AddAddress("fred.bail.becode@yopmail.com", "Fred Bail");
+                    $mail->AddAddress("fred.bail.becode@gmail.com", "Fred Bail");
                     $mail->AddAddress("$email", "$firstname $lastname");
                     $mail->SetFrom("$email", "$firstname $lastname");
                     
